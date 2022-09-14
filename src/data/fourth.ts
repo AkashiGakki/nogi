e64361e482c6d3dda27f46ee4f75a26fd341b816
Akashi <akashi_sai@163.com>
2022-09-14 18:52:18 +0800

    :tada: feat: member data fourth

--- src/data/fourth.ts
+++ src/data/fourth.ts
@@ -1,90 +1,123 @@
+import { getAge } from '@use-kit/functions'
+
 export const fourthOsu = [
   'ayame', 'kaki', 'mayu', 'saku', 'nao',
   'yuri', 'saya', 'rika', 'hayashi',
-  'seira', 'yuna', 'mio', 'kuromi', 'sayuka', 'miyu', 'rei',
+  'seira', 'yuna', 'miyu', 'sayaka', 'mio', 'kuromi', 'rei',
 ]
 
 export const fourthMember = [{
-  name: '弓木 奈於',
-  english_name: 'yumiki nao',
-  birthday: '1999/02/03',
-},
-{
-  name: '松尾 美佑',
-  english_name: 'matsuo miyu',
-  birthday: '2004/01/03',
-},
-{
-  name: '林 瑠奈',
-  english_name: 'hayashi runa',
-  birthday: '2003/10/02',
-},
-{
-  name: '佐藤 璃果',
-  english_name: 'sato rika',
-  birthday: '2001/08/09',
-},
-{
-  name: '黒見 明香',
-  english_name: 'kuromi haruka',
-  birthday: '2004/01/19',
-},
-{
-  name: '清宮 レイ',
-  english_name: 'seimiya rei',
-  birthday: '2003/08/01',
-},
-{
-  name: '北川 悠理',
-  english_name: 'kitagawa yuri',
-  birthday: '2001/08/08',
-},
-{
-  name: '金川 紗耶',
-  english_name: 'kanagawa saya',
-  birthday: '2001/10/31',
-
-},
-{
-  name: '矢久保 美緒',
-  english_name: 'yakubo mio',
-  birthday: '2002/08/14',
-},
-{
-  name: '早川 聖来',
-  english_name: 'hayakawa seira',
-  birthday: '2000/08/24',
-},
-{
-  name: '掛橋 沙耶香',
-  english_name: 'kakehashi sayaka',
-  birthday: '2002/11/20',
-},
-{
-  name: '賀喜 遥香',
-  english_name: 'kaki haruka',
-  birthday: '2001/08/08',
-},
-{
-  name: '筒井 あやめ',
-  english_name: 'tsutsui ayame',
-  birthday: '2004/06/08',
-},
-{
-  name: '田村 真佑',
-  english_name: 'tamura mayu',
-  birthday: '1999/01/12',
-},
-{
-  name: '柴田 柚菜',
-  english_name: 'shibata yuna',
-  birthday: '2003/03/03',
-},
-{
-  name: '遠藤 さくら',
-  english_name: 'endo sakura',
-  birthday: '2001/10/03',
-},
-]
+  id: '04-01',
+  use: 'tsutsui ayame',
+  nickname: 'ayame',
+  age: getAge('2004/06/08'),
+  height: 160,
+  active: true,
+}, {
+  id: '04-02',
+  use: 'kaki haruka',
+  nickname: 'kaki',
+  age: getAge('2001/08/08'),
+  height: 166,
+  active: true,
+}, {
+  id: '04-03',
+  use: 'tamura mayu',
+  nickname: '',
+  age: getAge('1999/01/12'),
+  height: 160,
+  active: true,
+}, {
+  id: '遠藤 さくら',
+  use: 'endo sakura',
+  nickname: '',
+  age: getAge('2001/10/03'),
+  height: 164,
+  active: true,
+}, {
+  id: '弓木 奈於',
+  use: 'yumiki nao',
+  nickname: '',
+  age: getAge('1999/02/03'),
+  height: 165,
+  active: true,
+}, {
+  id: '北川 悠理',
+  use: 'kitagawa yuri',
+  nickname: '',
+  age: getAge('2001/08/08'),
+  height: 165,
+  active: true,
+}, {
+  id: '金川 紗耶',
+  use: 'kanagawa saya',
+  nickname: '',
+  age: getAge('2001/10/31'),
+  height: 165,
+  active: true,
+}, {
+  id: '佐藤 璃果',
+  use: 'sato rika',
+  nickname: '',
+  age: getAge('2001/08/09'),
+  height: 160,
+  active: true,
+}, {
+  id: '林 瑠奈',
+  use: 'hayashi runa',
+  nickname: '',
+  age: getAge('2003/10/02'),
+  height: 164,
+  active: true,
+}, {
+  id: '早川 聖来',
+  use: 'hayakawa seira',
+  nickname: '',
+  age: getAge('2000/08/24'),
+  height: 164,
+  active: true,
+}, {
+  id: '柴田 柚菜',
+  use: 'shibata yuna',
+  nickname: '',
+  age: getAge('2003/03/03'),
+  height: 160,
+  active: true,
+}, {
+  id: '松尾 美佑',
+  use: 'matsuo miyu',
+  nickname: '',
+  age: getAge('2004/01/03'),
+  height: 167,
+  active: true,
+}, {
+  id: '掛橋 沙耶香',
+  use: 'kakehashi sayaka',
+  nickname: '',
+  age: getAge('2002/11/20'),
+  height: 154,
+  active: true,
+}, {
+  id: '矢久保 美緒',
+  use: 'yakubo mio',
+  nickname: '',
+  age: getAge('2002/08/14'),
+  height: 153,
+  active: true,
+}, {
+  id: '黒見 明香',
+  use: 'kuromi haruka',
+  nickname: '',
+  age: getAge('2004/01/19'),
+  height: 162,
+  active: true,
+}, {
+  id: '清宮 レイ',
+  use: 'seimiya rei',
+  nickname: '',
+  age: getAge('2003/08/01'),
+  height: 164,
+  active: true,
+}]
 
 export const fourthGraduated = []
