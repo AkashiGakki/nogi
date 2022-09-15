03dff96fe4100abb329cf056b317c9a9650146cd
Akashi <akashi_sai@163.com>
2022-09-15 09:21:51 +0800

    :tada: feat: member data second

--- src/data/second.ts
+++ src/data/second.ts
@@ -11,6 +11,7 @@ export const secondMember = [{
   use: 'suzuki ayane',
   nickname: 'ayane',
   age: getAge('1999/03/05'),
+  height: 160,
   active: true,
 }]
 
@@ -19,53 +20,62 @@ export const secondGraduated = [{
   use: 'hori miona',
   nickname: 'miona',
   age: getAge('1996/10/15'),
+  height: 160,
   active: false,
 }, {
   id: '02-02',
   use: 'sasaki kotoko',
   nickname: 'kotoko',
   age: getAge('1998/08/28'),
+  height: 163,
   active: false,
 }, {
   id: '02-03',
   use: 'kitano hinako',
   nickname: 'hinako',
   age: getAge('1996/07/17'),
+  height: 158,
   active: false,
 }, {
   id: '02-04',
   use: 'shinuchi mai',
   nickname: 'shinuchi',
   age: getAge('1992/01/22'),
+  height: 165,
   active: false,
 }, {
   id: '02-05',
   use: 'terada ranze',
   nickname: 'ranze',
   age: getAge('1998/09/23'),
+  height: 155,
   active: false,
 }, {
   id: '02-06',
   use: 'itou junna',
   age: getAge('1998/11/30'),
+  height: 166,
   active: false,
 }, {
   id: '02-07',
   use: 'yamazaki rena',
   nickname: 'yamazaki',
   age: getAge('1997/05/21'),
+  height: 164,
   active: false,
 }, {
   id: '02-08',
   use: 'watanabe miria',
   nickname: 'miria',
   age: getAge('1999/11/01'),
+  height: 153,
   active: false,
 }, {
   id: '02-10',
   use: 'matsui rena',
   nickname: 'rena',
   age: getAge('1991/07/27'),
+  height: 162,
   active: false,
 }, {
   id: '02-11',
@@ -78,6 +88,7 @@ export const secondGraduated = [{
   use: 'itou karin',
   nickname: 'karin',
   age: getAge('1993/05/26'),
+  height: 154,
   active: false,
 }, {
 
@@ -85,10 +96,13 @@ export const secondGraduated = [{
   use: 'yonetoku kyoka',
   nickname: 'kyoka',
   age: getAge('1999/04/14'),
+  height: -Infinity,
   active: false,
 }, {
   id: '02-14',
   use: 'yada risako',
   nickname: 'risako',
   age: getAge('1995/03/08'),
+  height: -Infinity,
+  active: false,
 }]
